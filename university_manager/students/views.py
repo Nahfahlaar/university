d4476a97b4784a4f8567bbeed3c54ddbfe190bfb
Nahfahlaar <kish95@mail.ru>
2024-10-24 15:18:01 +0000

    Удалил папку SPBGUPTD и добавил в доку квери параметр для GET запроса

--- university_manager/students/views.py
+++ university_manager/students/views.py
@@ -1,9 +1,19 @@
 from rest_framework import viewsets
 from .models import Student
 from .serializers import *
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
 
 class StudentViewSet(viewsets.ModelViewSet):
     queryset = Student.objects.all()
+
+
+
+    @swagger_auto_schema(manual_parameters=[
+        openapi.Parameter('as_admin', openapi.IN_QUERY, description="Введите 1 если хотите увидеть все поля", type=openapi.TYPE_STRING)
+    ])
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
 
     def get_serializer_class(self):
         if self.request.method in ['GET']:
